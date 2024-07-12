917c486f5881a4a0ae609abb4e4c7495a87d2d39
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2024-07-12 18:11:00 +0200

    new

--- Level 4/rev_wstr/rev_wstr.c
+++ Level 4/rev_wstr/rev_wstr.c
@@ -1,52 +1,39 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+char space(char c)
 {
-    int start; // Indice di inizio di una parola
-    int end; // Indice di fine di una parola
-    int i = 0; // Indice per scorrere la stringa
+	return(c == ' ' || c == '\t');
+}
 
-    if (argc == 2) // Verifica se è stato passato un solo argomento da riga di comando
-    {
-        // Calcola la lunghezza della stringa argv[1]
-        while (argv[1][i] != '\0')
-            i++;
+void	rev_str(char *str)
+{
+	int flag = 0;
+	int i = 0;
+	while(str[i] != '\0')
+		i++;
+	while(i >= 0)
+	{
+		while(space(str[i]) || str[i] == '\0')
+			i--;
+		int end = i;
+		while(!space(str[i]) && str[i])
+			i--;
+		int start = i + 1;
+		flag = start;
+		while(start <= end)
+		{
+			write(1, &str[start], 1);
+			start++;
+		}
+		if(flag > 0)
+			write(1, " ", 1);
+	}
+}
 
-        // Ciclo per stampare le parole al contrario
-        while (i >= 0)
-        {
-            // Salta eventuali spazi o tab all'inizio o alla fine della stringa
-            while (argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
-                i--;
-
-            // Memorizza l'indice di fine di una parola
-            end = i;
-
-            // Salta la parola
-            while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
-                i--;
-
-            // Memorizza l'indice di inizio di una parola
-            start = i + 1;
-
-            // Flag per tenere traccia dell'indice di inizio di una parola
-            int flag;
-            flag = start;
-
-            // Stampa la parola dalla fine all'inizio
-            while (start <= end)
-            {
-                write(1, &argv[1][start], 1);
-                start++;
-            }
-
-            // Se non è la prima parola, stampa uno spazio
-            if (flag != 0)
-            {
-                write(1, " ", 1);
-            }
-        }
-    }
-    write(1, "\n", 1); // Stampa un carattere di nuova riga
-    return 0;
+int main(int ac, char **av)
+{
+	if(ac==2)
+		rev_str(av[1]);
+	write(1, "\n", 1);
+	return 0 ;
 }
